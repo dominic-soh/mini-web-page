2944e6e95fbbe86fc0492e4e881cccecc4056944
Dominic Soh <dominic.soh@amptalk.co.jp>
2022-03-24 14:09:53 +0900

    More form abstraction

--- src/views/ContactMe.vue
+++ src/views/ContactMe.vue
@@ -1,5 +1,17 @@
 <template>
-  <generic-form></generic-form>
+  <generic-form>
+    <template #first-line>Name</template>
+    <template #second-line>Email</template>
+  </generic-form>
+  <textarea
+    v-model="text"
+    style="width: 12rem"
+    height="100"
+    :append-icon="text == '' ? 'person' : ''"
+    class="ml-5"
+    placeholder="Share your thoughts!"
+  />
+  <button @click="submitForm">Submit</button>
 </template>
 
 <script lang="ts">
